95ddfa2c872010d1996f6258917b5671e5f4c473
Unknown2k10 <emmanuelokafor39@gmail.com>
2023-10-12 12:30:13 +0100

    Play Fetch - Unit 2

---
+++ Play Fetch/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    public float fireDelay = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+      //  fireDelay -= 0.1f;
+        // On spacebar press, send dog
+        if (!Input.GetKeyDown(KeyCode.Space) && fireDelay <= 0.0f)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+        fireDelay = 1; 
+        }
+       // Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+       if (fireDelay >= 0) {
+        fireDelay -= Time.deltaTime;
+       }
+       // fireDelay = 10.0f;
+    }
+}
+
